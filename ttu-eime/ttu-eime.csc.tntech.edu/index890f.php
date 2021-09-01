d8bd1bff9594406ee304b15be07538850504af98
thill <thill@tntech.edu>
2021-09-01 12:17:52 -0500

    bulk files minus images

---
+++ ttu-eime/ttu-eime.csc.tntech.edu/index890f.php
@@ -0,0 +1,12 @@
+<?xml version="1.0"?>
+<?xml-stylesheet type="text/css" href="http://ttu-eime.csc.tntech.edu/skins/common/feed.css?303"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
+		<id>http://ttu-eime.csc.tntech.edu/index.php?title=Special:RecentChanges&amp;feed=atom</id>
+		<title>TTU-EIME  - Recent changes [en]</title>
+		<link rel="self" type="application/atom+xml" href="http://ttu-eime.csc.tntech.edu/index.php?title=Special:RecentChanges&amp;feed=atom"/>
+		<link rel="alternate" type="text/html" href="http://ttu-eime.csc.tntech.edu/index.php/Special:RecentChanges"/>
+		<updated>2021-04-16T16:26:02Z</updated>
+		<subtitle>Track the most recent changes to the wiki in this feed.</subtitle>
+		<generator>MediaWiki 1.19.1</generator>
+
+	</feed>
